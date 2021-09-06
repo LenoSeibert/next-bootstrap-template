b61c5706f22becb3fff4d7f1bf5a7ac79fa27c6d
Lenoir Seibert <siwebbr@gmail.com>
2021-09-06 10:49:56 -0300

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Layout from '../components/Layout'
+import Layout from '../../components/Layout'
 //import { Container, Row, Card, Button } from 'react-bootstrap'
 
 export default function Home() {
